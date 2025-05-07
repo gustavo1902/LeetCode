40f8b135d64b7a82dbb2aa49ee1151645909d70f
Gustavo M. Costa <101591580+gustavo1902@users.noreply.github.com>
2025-05-07 19:20:03 -0300

    feat: final solution

---
+++ python/is_valid.py
@@ -0,0 +1,15 @@
+def isValid(s: str) -> bool:
+    stack = []
+    pairs = {')': '(', '}': '{', ']': '['}
+    
+    for char in s:
+        if char in pairs.values():
+            stack.append(char)
+        elif char in pairs:
+            if not stack or stack[-1] != pairs[char]:
+                return False
+            stack.pop()
+        else:
+            return False 
+
+    return not stack
