bd6b4c5f8a8016cc25f20596c22b53617308263a
Gustavo M. Costa <101591580+gustavo1902@users.noreply.github.com>
2025-05-31 18:35:21 -0300

    feat: final solution

--- c/plus_one.c
+++ c/plus_one.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
     
     int* result = (int*)malloc((digitsSize + 1) * sizeof(int));
-    int carry = 1; 
+    int carry = 1;
     for (int i = digitsSize - 1; i >= 0; i--) {
         int sum = digits[i] + carry;
         result[i + 1] = sum % 10;
@@ -19,29 +16,4 @@ int* plusOne(int* digits, int digitsSize, int* returnSize) {
         *returnSize = digitsSize;
         return result + 1;
     }
-}
-
-int main() {
-    int digits[] = {9, 9, 9};
-    int digitsSize = sizeof(digits) / sizeof(digits[0]);
-    int returnSize;
-
-    int* result = plusOne(digits, digitsSize, &returnSize);
-
-    printf("Output: [");
-    for (int i = 0; i < returnSize; i++) {
-        printf("%d", result[i]);
-        if (i != returnSize - 1) {
-            printf(",");
-        }
-    }
-    printf("]\n");
-
-    if (returnSize == digitsSize + 1) {
-        free(result);
-    } else {
-        free(result - 1);
-    }
-
-    return 0;
 }
